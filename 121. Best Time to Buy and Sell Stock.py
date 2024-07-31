903660e4c8e728fa1137f119ba2cfbefdbc7a67b
Ilyas <ilyas.shamshiden@mail.ru>
2024-07-31 18:21:41 +0500

    Add #121 task

---
+++ 121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # count = 0
+        # for i in range(len(prices) - 1, 0, -1):
+        #     for j in range(i - 1, -1, -1):
+        #         if prices[i] - prices[j] > count:
+        #             count = prices[i] - prices[j]
+        # return count
+
+        
+        
+        
+        
+        
+
+a = Solution()
+print(a.maxProfit(prices = [2,4,1]))
